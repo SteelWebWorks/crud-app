49b825c62841dc990850383ee11662af2eda5901
Zsigmond G. Balázs <steelwebworks@gmail.com>
2024-08-08 13:30:20 +0200

    Change db connection to neon.tech

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'user',
-      password: 'egyedeM1268',
-      database: 'library',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<string>('DATABASE_TYPE') as any,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnathorized: false,
+          },
+        },
+        host: configService.get<string>('DATABASE_HOST'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<string>('ENVIORMENT') == 'dev',
+      }),
+      inject: [ConfigService],
     }),
     BooksModule,
     UsersModule,
